970566b493d6528fec058ad51b9fa80c717e7c36
Rachel Scherer <rscherer1@student.rccd.edu>
2019-09-27 17:58:40 -0700

    Started to create a navbar

--- src/App.js
+++ src/App.js
@@ -7,11 +7,81 @@ import { Projects } from "./Projects";
 import { Contact } from "./Contact";
 import { NoMatch } from "./NoMatch";
 import { Layout } from "./components/Layout";
+import styled from "styled-components";
+
+const Styles = styled.div`
+  div {
+    text-align:center;
+  }
+
+  .intro {
+    margin-top:200px;
+    text-align: center;
+  }
+
+  .nav{
+    width: 100%;
+    margin-top:-20px
+    justify-content: center;
+  }
+
+  .nav_text{
+    width: 100%
+    justify-content: space-around;
+    padding: 1rem;
+  }
+  
+  .nav_text h1{
+    font-size: 1.5rem;
+  }
+
+  .nav_text h1:hover {
+    color: blue;
+    cursor: pointer;
+  }
+
+  .scrolled{
+    position:fixed;
+    background-color: gray;
+    margin-top: -271px;
+  }
+`;
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      scrolled: false
+    };
+  }
+
+  componentDidMount() {
+    window.addEventListener("scroll", () => {
+      const isTop = window.scrollY < 280;
+      if (isTop !== true) {
+        this.setState({ scrolled: true });
+      } else {
+        this.setState({ scrolled: false });
+      }
+    });
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener("scroll");
+  }
+
   render() {
     return (
       <React.Fragment>
+        <Styles>
+          <h2 className="intro">hi, i'm rachel</h2>
+          <hr />
+          <div className={this.state.scrolled ? "nav scrolled" : "nav"}>
+            <div className="nav_text">
+              <h1>yay, i did it</h1>
+            </div>
+          </div>
+        </Styles>
         <Layout>
           <Router>
             <Switch>
