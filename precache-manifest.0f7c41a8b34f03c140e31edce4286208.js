969c735c02335408ad8a184c748bab1315a2c0eb
Rachel Scherer <rscherer1@student.rccd.edu>
2019-09-28 18:06:51 -0700

    Updates

--- precache-manifest.0f7c41a8b34f03c140e31edce4286208.js
+++ precache-manifest.0f7c41a8b34f03c140e31edce4286208.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "23a9f82d65d66902686a24f0c733215a",
+    "revision": "2908fe251d390c79f4ea01642cc3318a",
     "url": "/personal-website/index.html"
   },
   {
-    "revision": "c9ad72060834915a7e7b",
+    "revision": "879382dc9ba0088247e8",
     "url": "/personal-website/static/css/main.aa46ef81.chunk.css"
   },
   {
@@ -12,8 +12,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/personal-website/static/js/2.af1d4689.chunk.js"
   },
   {
-    "revision": "c9ad72060834915a7e7b",
-    "url": "/personal-website/static/js/main.fc08ceaf.chunk.js"
+    "revision": "879382dc9ba0088247e8",
+    "url": "/personal-website/static/js/main.9e5e6ba6.chunk.js"
   },
   {
     "revision": "0200ac9aed1a66eebda8",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/personal-website/precache-manifest.3ce361649b75a102c2da37753851e454.js"
+  "/personal-website/precache-manifest.0f7c41a8b34f03c140e31edce4286208.js"
 );
 
 self.addEventListener('message', (event) => {
